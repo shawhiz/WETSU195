b9942918792329002dbf7e9d7453ef0957b6da8a
shawhiz <shawhiz@gmail.com>
2018-07-15 14:53:48 -0400

    Trying to get navigation working

--- src/wetsu195/MainAppController.java
+++ src/wetsu195/MainAppController.java
@@ -5,9 +5,11 @@
  */
 package wetsu195;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.SplitPane;
 import javafx.scene.layout.AnchorPane;
@@ -26,14 +28,37 @@ public class MainAppController implements Initializable {
     @FXML
     private AnchorPane navHolder;
     @FXML
-    private AnchorPane contentHolder;
-
+    public  AnchorPane contentHolder;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
     }    
+    
+    public void showClients() throws IOException{
+        AnchorPane clientPane = FXMLLoader.load(getClass().getResource("View/Clients.fxml"));             
+        contentHolder.getChildren().setAll(clientPane);
+    }
+
+    public void showAppointments() throws IOException {
+       AnchorPane appointmentsPane = FXMLLoader.load(getClass().getResource("View/Appointments.fxml"));
+       contentHolder.getChildren().setAll(appointmentsPane);  
+    }
+
+    public void showCalendar() throws IOException {
+       AnchorPane calendarPane = FXMLLoader.load(getClass().getResource("View/Calendar.fxml"));
+       contentHolder.getChildren().setAll(calendarPane);  
+    }
+
+    public void showReports() throws IOException {
+       AnchorPane reportsPane = FXMLLoader.load(getClass().getResource("View/Reports.fxml"));
+       contentHolder.getChildren().setAll(reportsPane);  
+    }
+
+    public void signOut() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
--- src/wetsu195/View/LeftNavController.java
+++ src/wetsu195/View/LeftNavController.java
@@ -5,9 +5,20 @@
  */
 package wetsu195.View;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import wetsu195.MainAppController;
 
 /**
  * FXML Controller class
@@ -16,12 +27,65 @@ import javafx.fxml.Initializable;
  */
 public class LeftNavController implements Initializable {
 
+    @FXML
+    private AnchorPane root;
+    @FXML
+    private VBox menuHolder;
+    @FXML
+    private Button clients;
+    @FXML
+    private Button appointments;
+    @FXML
+    private Button calendar;
+    @FXML
+    private Button reports;
+    @FXML
+    private Button signout;
+
+    MainAppController mainController = new MainAppController();
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    public void showClients() throws IOException {
+        mainController.showClients();
+        clients.getStyleClass().add("active-nav");
+        appointments.getStyleClass().remove("active-nav");
+        calendar.getStyleClass().remove("active-nav");
+        reports.getStyleClass().remove("active-nav");
+    }
+
+    public void showAppointments() throws IOException {
+        mainController.showAppointments();
+        clients.getStyleClass().remove("active-nav");
+        appointments.getStyleClass().add("active-nav");
+        calendar.getStyleClass().remove("active-nav");
+        reports.getStyleClass().remove("active-nav");
+    }
+
+    public void showCalendar() throws IOException {
+        mainController.showCalendar();
+        clients.getStyleClass().remove("active-nav");
+        appointments.getStyleClass().remove("active-nav");
+        calendar.getStyleClass().add("active-nav");
+        reports.getStyleClass().remove("active-nav");
+    }
+
+    public void showReports() throws IOException {
+        mainController.showReports();
+        clients.getStyleClass().remove("active-nav");
+        appointments.getStyleClass().remove("active-nav");
+        calendar.getStyleClass().remove("active-nav");
+        reports.getStyleClass().add("active-nav");
+    }
+
+    public void signout() {
+        mainController.signOut();
+    }
+
 }
