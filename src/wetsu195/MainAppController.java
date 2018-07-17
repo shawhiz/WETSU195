05b50ac83550c92cd48b1d6d3002423b566a9d9a
shawhiz <shawhiz@gmail.com>
2018-07-17 18:38:16 -0400

    its working

--- src/wetsu195/MainAppController.java
+++ src/wetsu195/MainAppController.java
@@ -8,10 +8,13 @@ package wetsu195;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.SplitPane;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
 /**
@@ -19,7 +22,7 @@ import javafx.scene.layout.AnchorPane;
  *
  * @author shawh
  */
-public class MainAppController   implements Initializable{
+public class MainAppController implements Initializable {
 
     @FXML
     private AnchorPane root;
@@ -28,35 +31,85 @@ public class MainAppController   implements Initializable{
     @FXML
     private AnchorPane navHolder;
     @FXML
-    public  AnchorPane contentHolder;
-        
-    public void showClients() throws IOException{
-    
-        AnchorPane clientPane = FXMLLoader.load(getClass().getResource("Clients.fxml"));             
+    public AnchorPane contentHolder;
+    @FXML
+    private AnchorPane nav;
+    @FXML
+    private ImageView logo;
+    @FXML
+    private Button clients;
+    @FXML
+    private Button appointments;
+    @FXML
+    private Button calendar;
+    @FXML
+    private Button reports;
+    @FXML
+    private Button signout;
+
+    @FXML
+    public void showClients() throws IOException {
+        changeActiveLink(clients);
+        AnchorPane clientPane = FXMLLoader.load(getClass().getResource("Clients.fxml"));
         contentHolder.getChildren().setAll(clientPane);
     }
 
+    @FXML
     public void showAppointments() throws IOException {
-       AnchorPane appointmentsPane = FXMLLoader.load(getClass().getResource("Appointments.fxml"));
-       contentHolder.getChildren().setAll(appointmentsPane);  
+        changeActiveLink(appointments);
+        AnchorPane appointmentsPane = FXMLLoader.load(getClass().getResource("Appointments.fxml"));
+        contentHolder.getChildren().setAll(appointmentsPane);
     }
 
+    @FXML
     public void showCalendar() throws IOException {
-       AnchorPane calendarPane = FXMLLoader.load(getClass().getResource("Calendar.fxml"));
-       contentHolder.getChildren().setAll(calendarPane);  
+        changeActiveLink(calendar);
+        AnchorPane calendarPane = FXMLLoader.load(getClass().getResource("Calendar.fxml"));
+        contentHolder.getChildren().setAll(calendarPane);
     }
 
+    @FXML
     public void showReports() throws IOException {
-       AnchorPane reportsPane = FXMLLoader.load(getClass().getResource("Reports.fxml"));
-       contentHolder.getChildren().setAll(reportsPane);  
+        changeActiveLink(reports);
+        AnchorPane reportsPane = FXMLLoader.load(getClass().getResource("Reports.fxml"));
+        contentHolder.getChildren().setAll(reportsPane);
     }
 
     public void signOut() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
     }
+
     
+    @FXML
+    private void changeActiveLink(Button button) {
+        if (button == clients) {
+            clients.getStyleClass().add("active-nav");
+        } else {
+            clients.getStyleClass().remove("active-nav");
+        }
+
+        if (button == appointments) {
+            appointments.getStyleClass().add("active-nav");
+        } else {
+            appointments.getStyleClass().remove("active-nav");
+        }
+
+        if (button == calendar) {
+            calendar.getStyleClass().add("active-nav");
+        } else {
+            calendar.getStyleClass().remove("active-nav");
+        }
+
+        if (button == reports) {
+            reports.getStyleClass().add("active-nav");
+        } else {
+            reports.getStyleClass().remove("active-nav");
+        }
+
+    }
+
 }
